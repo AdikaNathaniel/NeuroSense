33d4cc87e07700acaa277080465491fa74b76932
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-11-01 02:15:07 +0000

    email-updated

--- neuro_sense/src/dto/create-user.dto.ts
+++ neuro_sense/src/dto/create-user.dto.ts
@@ -12,6 +12,10 @@ export class CreateUserDto {
 
   @IsString()
   @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
   GhanaCard: string;
 
   @IsString()
--- neuro_sense/src/dto/forgot-password.dto.ts
+++ neuro_sense/src/dto/forgot-password.dto.ts
@@ -1,7 +1,7 @@
-import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class ForgotPasswordDto {
-  @IsString()
+  @IsEmail()
   @IsNotEmpty()
-  username: string;
+  email: string; 
 }
