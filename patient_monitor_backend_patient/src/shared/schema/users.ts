bd73d100b9a7b64d051616efe9fcf17699f4675f
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-12-05 17:33:11 +0000

    feat:added a neurosense-card

--- patient_monitor_backend_patient/src/shared/schema/users.ts
+++ patient_monitor_backend_patient/src/shared/schema/users.ts
@@ -28,7 +28,7 @@ export class Users extends Document {
   email: string;
 
   @Prop({ required: true })
-  card: string;
+  NeuroSenseCard: string;
 
   @Prop({ required: false, unique: true })
   GhanaCard: string;
